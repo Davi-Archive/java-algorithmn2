e93a9992c6a6ad92af187596f0cd3cde742ba9a8
d1av <davi4alves@gmail.com>
2022-11-22 15:59:20 -0300

    feat: add new employee algorithmn

---
+++ Udemy009/src/application/Employee.java
@@ -0,0 +1,47 @@
+package application;
+
+public class Employee {
+	private Integer id;
+	private String name;
+	private Double salary;
+
+	public Employee(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public void salaryIncrease(int salaryIncreasePercentage) {
+		salary = salary + (salary / 100 * salaryIncreasePercentage);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return id + ", " + name + ", " + salary;
+	}
+
+}
--- Udemy009/src/application/Program.java
+++ Udemy009/src/application/Program.java
@@ -7,18 +7,43 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		List<Object> list = new ArrayList<>();		
+		List<Employee> list = new ArrayList<>();
 		int listSize;
-		
+
 		System.out.println("How many employees will be registered");
-		
-		
-		for(int i=0;i<listSize;i++) {
-			
+		listSize = scanner.nextInt();
+
+		for (int i = 1; i <= listSize; i++) {
+			System.out.printf("Employee # %d %n ", i);
+
+			System.out.printf("Id ");
+			int id = scanner.nextInt();
+
+			System.out.printf("Name ");
+			String name = scanner.next();
+
+			System.out.printf("Salary ");
+			Double salary = scanner.nextDouble();
+
+			list.add(new Employee(id, name, salary));
 		}
-		
+
+		System.out.printf("Enter the employee id that will have salary increase: ");
+		int idEmployee = scanner.nextInt();
+		Employee selectedEmployee = list.stream().filter(x -> x.getId() == idEmployee).findFirst().orElse(null);
+
+		System.out.printf("Enter the percentage: ");
+		int salaryIncreasePercentage = scanner.nextInt();
+
+		selectedEmployee.salaryIncrease(salaryIncreasePercentage);
+
+		System.out.println("List of Employees:");
+		for (Employee employee : list) {
+			System.out.println(employee.toString());
+		}
+
 	}
 
 }
