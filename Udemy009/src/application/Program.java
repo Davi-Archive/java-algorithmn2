0dba6f1f6a631f97f617103a03c436fca9cf9382
d1av <davi4alves@gmail.com>
2022-11-22 15:28:49 -0300

    feat: employees algorithmn

---
+++ Udemy009/src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		List<Object> list = new ArrayList<>();		
+		int listSize;
+		
+		System.out.println("How many employees will be registered");
+		
+		
+		for(int i=0;i<listSize;i++) {
+			
+		}
+		
+	}
+
+}
