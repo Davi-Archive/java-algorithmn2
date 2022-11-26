cbb6d7ad8e7f2dd7a5f9e96c6dccf2274fd7b190
d1av <davi4alves@gmail.com>
2022-11-26 10:39:48 -0300

    feat: brazilian interest tax

---
+++ Udemy022/src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Amount: ");
+		double amount = scanner.nextDouble();
+		System.out.println("Months: ");
+		
+	}
+
+}
