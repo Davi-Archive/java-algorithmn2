aa3861b0f5bed2ad753b2877421ded0d9405e558
d1av <davi4alves@gmail.com>
2022-12-19 18:36:45 -0300

    feat: add tests for Financing class

--- UnitTest002/src/test/java/org/example/entities/FinancingTests.java
+++ UnitTest002/src/test/java/org/example/entities/FinancingTests.java
@@ -61,17 +61,56 @@ public class FinancingTests {
         Assertions.assertEquals(newIncome, fin.getIncome());
 
     }
+
     @Test
-    public void shouldSetTotalIncomeWhenDataIsValid() {
+    public void shouldThrowWhenIncomeIsInvalid() {
         Financing fin = FinancingFactory.createValidFinancing();
-        Double newIncome = 2500.0;
 
-        fin.setIncome(newIncome);
+        Double newIncome = 1200.0;
 
-        Assertions.assertEquals(newIncome, fin.getIncome());
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            fin.setIncome(newIncome);
+        });
 
     }
 
+    @Test
+    public void shouldChangeMonthsWhenDataIsValid() {
+        Financing fin = FinancingFactory.createValidFinancing();
+        Integer newMonths = 100;
+
+        fin.setMonths(newMonths);
+
+        Assertions.assertEquals(newMonths, fin.getMonths());
 
 
+    }
+
+    @Test
+    public void shouldThrowWhenMonthIsInvalid() {
+        Financing fin = FinancingFactory.createValidFinancing();
+        Integer newMonths = 12;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            fin.setMonths(newMonths);
+        });
+    }
+
+    @Test
+    public void shouldCalculateTheCorrectEntryValue() {
+        Financing fin = FinancingFactory.createValidFinancing();
+
+        Double totalAmount = fin.getTotalAmount();
+
+        Assertions.assertEquals(fin.entry(), totalAmount * 0.2);
+    }
+
+    @Test
+    public void shouldCalculateTheCorrectQuota(){
+        Financing fin = FinancingFactory.createValidFinancing();
+
+        Double calcQuota = (fin.getTotalAmount() - fin.getTotalAmount()*0.2) / fin.getMonths();
+
+        Assertions.assertEquals(fin.quota(),calcQuota);
+    }
 }
